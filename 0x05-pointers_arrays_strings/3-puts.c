26e07957b57617e42301ecd5eacd26c1d61b3f24
patiencesimuli <patiencesimuli19@gmail.com>
2022-09-20 04:00:24 -0700

    Made changes to errors

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,12 +2,12 @@
 /**
   * _puts - puts a string
   * 
-  * @str: string to print
+  * @str: String to print
   */
 void _puts(char *str)
 {
 	int i;
-
+	
 	for (i = 0; *(str + i) != '\0'; i++)
 	{
 		putchar(*(str + i));
