ea1e49602b28b1f3e4c5ed2b81a48da6e3012823
patiencesimuli <patiencesimuli19@gmail.com>
2022-09-20 04:12:26 -0700

    Created a new file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  * print_rev - reverse a string
+  * @s: string to print
+  */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = _strlen(s) - 1; i >= 0; i--)
+	{
+		putchar(*(s + i));
+	}
+	putchar('\n');
+}
