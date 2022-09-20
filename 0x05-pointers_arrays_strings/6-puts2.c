4cc0f9f98584610ec87bee2940cf83e49ae0c1ba
patiencesimuli <patiencesimuli19@gmail.com>
2022-09-20 08:52:42 -0700

    C code to put string twice

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "2-strlen.c"
+#include <stdio.h>
+/**
+  * puts2 - puts string twice
+  * @str: string to put twice
+  */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i < _strlen(str); i++)
+	{
+		if (i % 2 == 0)
+		{
+			putchar(str[i]);
+		}
+	}
+	putchar('\n');
+}
