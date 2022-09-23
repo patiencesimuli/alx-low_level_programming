7f6441b51ab4e4abce37e17706ff9d6d80844efe
patiencesimuli <patiencesimuli19@gmail.com>
2022-09-23 10:34:00 -0700

    Finally understood and made changes to file 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,18 @@
 #include "main.h"
 /**
-  * main - main function
-  * Return: 0
+  * main - Entry point
+  * Return: Always returns 0
   */
 int main(void)
 {
-	_putchar("_putchar");
-	_putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
